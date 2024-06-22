83d2b9799c12c0343442bb4e1f177ca85f2a9e87
aaronwarsop <86020594+aaronwarsop@users.noreply.github.com>
2024-06-22 17:19:05 +0100

    more testing gh pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/https://github.com/aaronwarsop/AaronWarsop.github.io/',
+  //base: '/https://github.com/aaronwarsop/aaronwarsop.github.io/',
 });
